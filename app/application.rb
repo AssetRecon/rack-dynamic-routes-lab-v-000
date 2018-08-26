936b2fb69968245d36c8113f3f8f7076970cf3ea
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:36:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,25 @@
 class Application
-@@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
-def call (env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
 
-  if req.path.match(/items/)
-    item_name = req.path.split("/items/").last
-    if item =@@items.find do |i|
-      i.name == item_name
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status=404
+      resp.write "Route not found"
     end
-    resp.write item.price
-  else
-    resp.write "Rout not found"
-    resp.status = 404
+    resp.finish
   end
-  else
-    resp.write "Rout not found"
-    resp.status = 404
-  end
+
 end
 
 
