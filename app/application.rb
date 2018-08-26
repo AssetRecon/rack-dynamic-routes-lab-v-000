126e8b6628761bc695f194153f7cbfcec6c4d910
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:26:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
 class Application
-
+@@items
 def call (env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
-  if req.path.match("items")
+  if req.path.match(/items/)
+    item = req.path.split("/items/").last
     resp.write "#{item.price}"
   else
     resp.write "Rout not found"
