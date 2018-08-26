a58037d902ca022041f50f59e582f44dd4731abc
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:34:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ def call (env)
   req = Rack::Request.new(env)
 
   if req.path.match(/items/)
-    item = req.path.split("/items/").last
-    if item == @@items.find do |item|
-      item.name == item
+    item_name = req.path.split("/items/").last
+    if item == @@items.find do |i|
+      i.name == item
     end
     resp.write item.price
   else
