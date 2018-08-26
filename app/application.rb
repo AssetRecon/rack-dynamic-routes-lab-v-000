891e67c3406fb83e98d0b5a3bcebe741b93fffa1
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:19:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@ class Application
 def call (env)
   resp = Rack::Reponse.new
   req = Rack::Request.new(env)
+
+  if req.path.match(\items\)
+
+  else
+    resp.write "Rout not found"
+    respt.status = 404
+  end
 end
 
 
