bc481068732a151d97e79befaaf5c2cdfd52b158
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:36:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ def call (env)
 
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-    if item == @@items.find do |i|
+    if item =@@items.find do |i|
       i.name == item_name
     end
     resp.write item.price
